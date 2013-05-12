23f4a80415dc7ba5781f0db2f5cd7db1d2637e8b
Becktor <JonathanBecktor@gmail.com>
2013-05-12 23:41:44 +0200

    raappooorrt diskdddddd

--- src/planner/app/Activity.java
+++ src/planner/app/Activity.java
@@ -6,20 +6,18 @@ import java.util.List;
 
 public class Activity implements Comparable<Activity> {
 
-	public static final int DEFAULT_WORK_HOURS = 0;
 	public static final double DEFAULT_ALL_WORK_HOURS = 0;
 	public static final String
 			DEFAULT_NAME = "Unnamed",
-			MSG_DATE_MISMATCH = "End date must be after start date.",
-			MSG_DUPE_USER = "The list already contains this user.",
+			MSG_EARLY_END_DATE = "End date must be after start date.",
+			MSG_LATE_START_DATE = "Start date must be before end date.",
 			MSG_NEG_ALL_HOURS = "Allocated work hours must be non-negative.",
 			MSG_NEG_WORK_HOURS = "Work hours must be non-negative.",
 			MSG_NON_POS_HOURS = "Registered work hours must be positive.",
 			MSG_NULL_NAME = "Name must not be null",
-			MSG_NULL_USER = "Users must not be null";
+			MSG_USER_DUPLICATE = "The list already contains this user.";
 
-
-	private double allocatedWorkHours, workHours = DEFAULT_WORK_HOURS;
+	private double allocatedWorkHours, workHours = 0;
 	private String name, description;
 	private Date startDate, endDate; 
 	private List<User> users = new ArrayList<User>();
@@ -30,10 +28,7 @@ public class Activity implements Comparable<Activity> {
 		else
 			this.name = DEFAULT_NAME;
 		this.description = description;
-		if(allocatedWorkHours >= 0)
-			this.allocatedWorkHours = allocatedWorkHours;
-		else
-			this.allocatedWorkHours = DEFAULT_ALL_WORK_HOURS;
+		this.allocatedWorkHours = allocatedWorkHours;
 	}
 
 	public Activity(String name, String description) {
@@ -49,13 +44,10 @@ public class Activity implements Comparable<Activity> {
 	}
 
 	public void assignUser(User user) throws OperationNotAllowedException {
-		if(user != null) {
-			if(!containsUser(user))
-				users.add(user);
-			else
-				throw new OperationNotAllowedException(Operation.ACT_ASSIGN_USER, MSG_DUPE_USER);
-		} else
-			throw new OperationNotAllowedException(Operation.ACT_ASSIGN_USER, MSG_NULL_USER);
+		if(!containsUser(user))
+			users.add(user);
+		else
+			throw new OperationNotAllowedException(Operation.ACT_ASSIGN_USER, MSG_USER_DUPLICATE);
 	}
 
 	@Override
@@ -67,6 +59,7 @@ public class Activity implements Comparable<Activity> {
 		return users.contains(user);
 	}
 
+	//Untested
 	public double getAllocatedWorkHours() {
 		return allocatedWorkHours;
 	}
@@ -126,24 +119,31 @@ public class Activity implements Comparable<Activity> {
 	}
 
 	public void setEndDate(Date endDate) throws OperationNotAllowedException {
-		if(endDate == null || startDate == null || endDate.after(startDate))
+		if(hasStartDate()) {
+			if(getStartDate().before(endDate))
+				this.endDate = endDate;
+			else
+				throw new OperationNotAllowedException(Operation.ACT_SET_END_DATE, MSG_EARLY_END_DATE);
+		} else 
 			this.endDate = endDate;
-		else
-			throw new OperationNotAllowedException(Operation.ACT_SET_END_DATE, MSG_DATE_MISMATCH);
 	}
 
+	//Should this throw an exception?
 	public void setName(String name) throws OperationNotAllowedException {
 		if(name != null)
 			this.name = name;
 		else
 			throw new OperationNotAllowedException(Operation.ACT_SET_NAME, MSG_NULL_NAME);
 	}
-
+  
 	public void setStartDate(Date startDate) throws OperationNotAllowedException {
-		if (startDate == null || endDate == null || startDate.before(endDate))
-			this.startDate = startDate;
-		else
-			throw new OperationNotAllowedException(Operation.ACT_SET_START_DATE, MSG_DATE_MISMATCH);
+		if(hasEndDate()) {
+			if (getEndDate().after(startDate))
+				this.startDate = startDate;
+			else
+				throw new OperationNotAllowedException(Operation.ACT_SET_START_DATE, MSG_LATE_START_DATE);
+		} else
+			this.startDate = startDate; 
 	}
 
 	public void setWorkHours(double workHours) throws OperationNotAllowedException {
